8ca1be216d2db9dabd59780cc12888dff9dddd98
KUTRALEESWARAN NATTAMAI HARIKRISHNAN <152646370+leeIITM@users.noreply.github.com>
2026-07-19 12:36:26 +0530

    Add: combinations - submission-3

---
+++ Data Structures & Algorithms/combinations/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        out = []
+        nums = [i+1 for i in range(n)]
+        
+        def f(ind, left , l):
+            if left == 0:
+                out.append(l[:])
+            if ind >= len(nums):
+                return
+            # Pick
+            l.append(nums[ind])
+            
+            f(ind + 1, left - 1 ,l)
+
+            # Not pick
+            l.pop()
+            f(ind + 1, left , l)
+        
+        f(0,k,l = [])
+        o = []
+        for i in out:
+            if i not in o:
+                o.append(i)
+            else:
+                pass
+        return o
