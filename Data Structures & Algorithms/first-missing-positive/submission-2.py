d10bb331e44a2e77ebe09cce7980fd8dc4ea502e
KUTRALEESWARAN NATTAMAI HARIKRISHNAN <152646370+leeIITM@users.noreply.github.com>
2026-07-31 00:47:30 +0530

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        # This is O(NlogN)
+        nums.sort()
+        mini =1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                if nums[i] == mini:
+                    mini +=1
+                elif nums[i] < mini :
+                    pass
+                else:
+                    return mini
+        return max(nums) + 1 if max(nums) + 1 > 0 else 1
+
+        
